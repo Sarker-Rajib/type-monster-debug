055f137765a0c5c92e6be2b3d75d665b44287e7c
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-09-15 09:54:32 +0600

    responsive history card and integer coount in history

--- js/history.js
+++ js/history.js
@@ -4,10 +4,12 @@ function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
 
+  const duration = parseInt(timeTaken);
+
   newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
-  <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
+  <p>You took: <span class="bold">${parseInt(duration)}</span> seconds</p>
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
   </div>
   `;
@@ -15,7 +17,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   histories.appendChild(newRow);
 
   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-  previousTests.push({ questionText, timeTaken, errorCount });
+  previousTests.push({ questionText, duration, errorCount });
   localStorage.setItem("testHistory", JSON.stringify(previousTests));
 
   displayHistory();
@@ -30,10 +32,10 @@ function displayHistory() {
     newRow.classList.add("card");
 
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
-  `;
+      <h3 class="pb-15">${test.questionText}</h3>
+      <p>You took <span class="bold">${test.duration}</span> seconds</p>
+      <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+    `;
 
     histories.appendChild(newRow);
   });
